66bed18cbc67e12c277a9dce22cd6bda44912325
max <maxjarinovsky@gmail.com>
2022-01-17 11:00:51 +0200

    second lesson

--- basics.ts
+++ basics.ts
@@ -41,12 +41,111 @@ function printOut(value: any): void{ // void means that this function will never
 }
 
 // Generics
-function insert<T>(array: T[], value:T){ 
+function insert<T>(array: T[],  value:T){ 
     // if we want to provide any type in the array and value, but they need to have the same type
     const newArr = [...array, value];
     return newArr;
 }
 const demoArr = [1, 2, 3];
 insert(demoArr, -1); // [1, 2, 3, -1]
+
+let list: number[] = [1, 2, 3]; 
+
+let tupleType: [string, number] = ["hello", 10];
+// tupleType = [10, "hello"] - error
+
+// never type 
+const error = () : never => {
+    throw new Error('Something went wrong')
+} 
+const infiniteLoop = () : never => {
+    while(true) {
+        console.log('hello')
+    }
+}
+// never says that function returns an error or has an infinite loop
+// void type says that function does not returns data
+
+//Enum type
+enum Directions {
+    Up,
+    Down,
+    Left,
+    Right 
+}
+Directions[0] //Up
+Directions[1] //Down
+Directions[2] //Left
+Directions[3] //Right
+// enum elements have indexes, but we can change them
+
+enum Directions1 {
+    Up = 2,
+    Down = 4,
+    Left = 6,
+    Right 
+}
+Directions[2] //Up
+Directions[4] //Down
+Directions.Left //6
+Directions[7] //Right - previous element's index + 1
+// we can get elements not only by keys, but also by values! 
+
+
+const createPassword = (name: string, age: number): string => `${name}${age}`;
+
+//createPassword('Max') - will cause an error, because age is not provided
+
+const createPassword1 = (name: string, age?: number) : string => `${name}${age}`;
+//createPassword1('Max') - will not cause an error, because age now is optional argument 
+
+// simple class in typescript
+class User {
+    public name: string; // public - the access is free
+    private age: number; // private - property or method is not available outside this class 
+    protected nickname: string; // protected - proprety or method is available only in class and extended classes
+    readonly pass: number; // readonly - read only proprety
+
+    constructor(name: string, age:number, nickname: string, pass: number){
+        this.name = name;
+        this.age = age;
+        this.nickname = nickname;
+        this.pass = pass; 
+    }
+}
+
+class User1 {
+    private name: string = 'Maxim Yarinovsky';
+}
+// in this case we do not need to call constructor method
+// User class, but less code:
+class User2 {
+    static userClass: string = 'A';// this property will be in every class example 
+    constructor(
+    public name: string, 
+    private age: number, 
+    protected nickname: string, 
+    readonly pass: number 
+    ){}
+
+    setAge(age: number){
+        this.age = age;
+    }
+}
+// in this case property's modificator(public, private ...) is obligatory
+ 
+ 
+namespace Names {
+    export const max: string = 'Max';
+    export const sasha: string = 'Sasha'; // export variables from namespace
+    const misha: string = 'Misha'; // misha const is private variable
+}
+const maxSasha: string = `${Names.max}${Names.sasha}`
+
+// we can create namespace in the special file and use it across the app
+// "///<reference path='Names.ts'>" - imropt namespace into another file
+
+
+
 
 
